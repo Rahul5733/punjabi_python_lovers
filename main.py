be0ae2cb21fe6f67a00bea117fc3c29a358b8b66
ubuntu turorial <rahul.sood94@gmail.com>
2021-10-18 16:31:23 +0200

    main.py added, reday to go

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/kuldeepmanak")
+def hello_world():
+    return "<h1>cheti kar serwan puttra!</h1><h2>wdia gana!</h2><p>written by totu abd sabng by the legend Kuldeep manak!</p>"
+
+@app.route("/sujeetpatar")
+def hello_world2():
+    return "<h1>cheti kar serwan puttra!</h1><h2>wdia gana!</h2><p>written by totu abd sabng by the legend sujeet patar!</p>"
