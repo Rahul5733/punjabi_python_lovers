0849e33277b0ccc370f91a123d0ec8239320b715
Rahul <35797085+Rahul5733@users.noreply.github.com>
2021-10-18 16:35:57 +0200

    manmohan waris api added

--- main.py
+++ main.py
@@ -9,3 +9,7 @@ def hello_world():
 @app.route("/sujeetpatar")
 def hello_world2():
     return "<h1>cheti kar serwan puttra!</h1><h2>wdia gana!</h2><p>written by totu abd sabng by the legend sujeet patar!</p>"
+
+@app.route("/manmohanwaris")
+def hello_world3():
+    return "<h1>cheti kar serwan puttra!</h1><h2>wdia gana!</h2><p>written by totu abd sabng by the legend sujeet patar!</p>"
